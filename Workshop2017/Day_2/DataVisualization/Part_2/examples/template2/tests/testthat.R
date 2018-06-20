11bb07d5cb4239b6cdee7083b23ffddcdd17b786
Marcus Davy <mdavy86@users.noreply.github.com>
2018-06-20 23:42:44 +1200

    improve testthat script

--- Workshop2017/Day_2/DataVisualization/Part_2/examples/template2/tests/testthat.R
+++ Workshop2017/Day_2/DataVisualization/Part_2/examples/template2/tests/testthat.R
@@ -1,36 +1,14 @@
-#!/bin/env Rscript
+#!/usr/bin/env Rscript
+#
+# Session -> Set Working Directory -> To Source File Location
+# then source this file
+#
 
 suppressPackageStartupMessages(library(testthat))
 
+suppressPackageStartupMessages(source("../utils.R"))
 
-## function to run testthat tests on code in a 'tests/testthat' directory
-test_code <- function(path, package = NULL, filter = NULL, reporter="summary") {
+test_database <- "./data/lsb.acct.sqlite"
 
-  if(!is.null(package)) {
-    for(i in package) {
-      suppressPackageStartupMessages(require(i, character.only = TRUE))
-    }
-  }
-
-  test_path <- "testthat"
-  if(!file_test("-d", test_path)) {
-    stop("No tests found for ", path, call. = FALSE)
-  }
-
-  reporter <- testthat:::find_reporter(reporter)
-  env <- testthat:::test_pkg_env(test_path)
-  testthat:::with_top_env(env, {
-    df <- test_dir(test_path, reporter = reporter, env = env,
-                   filter = filter)
-  })
-  if (!is.null(reporter$failed)) {
-    stop("Test failures", call. = FALSE)
-  }
-  invisible(df)
-}
-
-
-packages <- c("RCurl")
-
-## test_dir could be an option here
-test_code(".", package=packages)
+test_dir(path = "./testthat",
+reporter = ifelse(length(commandArgs(trailingOnly = TRUE)) == 0, "progress", "summary"))
